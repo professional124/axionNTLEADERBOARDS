501534d1f3f8e8979163d9b8ff62f3e5072e5fd5
nincaleb <caleb.glen.thomas@icloud.com>
2025-04-16 18:06:39 -0700

    Update nitrotype_leaderboard.py

--- nitrotype_leaderboard.py
+++ nitrotype_leaderboard.py
@@ -32,7 +32,7 @@ TEAM_TAGS = [
     "XMVP", "HFE", "TVM", "IR", "AV", "XBTP", "42712", "TDTY",
     "FERAL", "MEYBO", "LZNT", "XIII", "KNTT", "FTHM", "L3JENS", "C0NQUE", "BAR0NS",
     "WERTQY", "TFV", "WP", "QC", "IR", "PIRC", "FTHM", "1STRED", "FFS", "RR", "VIELLE", "VS",
-    "SWF", "CAT4", "NTY", "CS", "TMW", "ZL", "SBD2"
+    "SWF", "CAT4", "NTY", "CS", "TMW", "ZL", "SBD2", "TXT", "EMPERR"
 ]
 TEAM_TAGS = sorted(list(set(TEAM_TAGS)), key=TEAM_TAGS.index)
 
